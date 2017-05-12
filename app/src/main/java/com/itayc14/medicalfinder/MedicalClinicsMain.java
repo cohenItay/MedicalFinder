48d657e34bb7e16e654959f0da8ef17d95546e1a
itaycohen <itay.c14@gmail.com>
2017-05-12 09:53:28 +0300

    i just made a change from android studio

--- app/src/main/java/com/itayc14/medicalfinder/MedicalClinicsMain.java
+++ app/src/main/java/com/itayc14/medicalfinder/MedicalClinicsMain.java
@@ -42,7 +42,7 @@ public class MedicalClinicsMain extends AppCompatActivity implements OnClinicCli
     private final static int LOCATION_PERMISSION_CODE = 24;
     private DistanceViewUpdater dvu;
     private boolean isLandscape;
-    //hi there
+    //hi there i made change from android studio
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
